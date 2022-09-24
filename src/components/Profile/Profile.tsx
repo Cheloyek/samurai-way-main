de54de5fc5dd4249f37f9e92d5f34d27908d63ce
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-09-24 19:28:30 +0300

    add Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
         <div>
             <img src="https://media.istockphoto.com/photos/abstract-curved-shapes-picture-id1340367001?b=1&k=20&m=1340367001&s=170667a&w=0&h=rEDszAkp7zRWyHBRPcIwFecc0QlbZNhc0sM62rjAa3U=" alt=""/>
         </div>
