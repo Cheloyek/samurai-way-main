554313ae8934256f9c1147a4386125fa413f534d
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-09-24 20:36:23 +0300

    add routing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,37 +1,23 @@
 import React from "react";
 import s from './Navbar.module.css'
 
-/*let s = {
-    'nav': 'Navbar_nav__SuIdW',
-    'item': 'Navbar_item__EYY\+R'
-
-  let class1 = 'item'
-  let class2 = 'active'
-
-  let classes = class1 + ' ' + class2 // item active
-  or
-  let classes = `${class1} ${class2}` // item active
-  or
-  let classes = `${s.item} ${s.active}` // item active
-} */
-
 //check className in browser
 const Navbar = () => {
     return <nav className={s.nav}>
-        <div>
-            <a>Profile</a>
+        <div className={s.nav}>
+            <a href='/profile'>Profile</a>  {/*routing переход на страницу profile*/}
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href='/dialogs'>Messages</a>
         </div>
         <div className={s.nav}>
-            <a>News</a>
+            <a href='/news'>News</a>
         </div>
         <div className={s.nav}>
-            <a>Music</a>
+            <a href='/music'>Music</a>
         </div>
         <div className={s.nav}>
-            <a>Settings</a>
+            <a href='/settings'>Settings</a>
         </div>
     </nav>
 }
